be688b76f1ff78401774398d424589aeeb9e42db
Artem Melnychuk <artem.melnychuk@tum.de>
2022-12-13 15:26:38 +0100

    first commit

---
+++ 2022_07_10-21_40_21_711235_ex09_u0523/exercise_code/networks/keypoint_nn.py
@@ -0,0 +1,121 @@
+"""Models for facial keypoint detection"""
+
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+# TODO: Choose from either model and uncomment that line
+# class KeypointModel(nn.Module):
+class KeypointModel(pl.LightningModule):
+    """Facial keypoint detection model"""
+    def __init__(self, hparams):
+        """
+        Initialize your model from a given dict containing all your hparams
+        Warning: Don't change the method declaration (i.e. by adding more
+            arguments), otherwise it might not work on the submission server
+            
+        NOTE: You could either choose between pytorch or pytorch lightning, 
+            by switching the class name line.
+        """
+        super().__init__()
+        self.save_hyperparameters(hparams)
+        ########################################################################
+        # TODO: Define all the layers of your CNN, the only requirements are:  #
+        # 1. The network takes in a batch of images of shape (Nx1x96x96)       #
+        # 2. It ends with a linear layer that represents the keypoints.        #
+        # Thus, the output layer needs to have shape (Nx30),                   #
+        # with 2 values representing each of the 15 keypoint (x, y) pairs      #
+        #                                                                      #
+        # Some layers you might consider including:                            #
+        # maxpooling layers, multiple conv layers, fully-connected layers,     #
+        # and other layers (such as dropout or batch normalization) to avoid   #
+        # overfitting.                                                         #
+        #                                                                      #
+        # We would truly recommend to make your code generic, such as you      #
+        # automate the calculation of the number of parameters at each layer.  #
+        # You're going probably try different architecutres, and that will     #
+        # allow you to be quick and flexible.                                  #
+        ########################################################################
+        
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=4, padding=2),
+            nn.ELU(),
+            nn.MaxPool2d(4),
+            nn.Dropout(0.4),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, padding=4),
+            nn.ELU(),
+            nn.MaxPool2d(4),
+            nn.Dropout(0.4),
+            nn.Flatten(),
+            nn.Linear(3136, 30),
+            nn.Linear(30, 30)
+        )
+
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+
+    def forward(self, x):
+        
+        # check dimensions to use show_keypoint_predictions later
+        if x.dim() == 3:
+            x = torch.unsqueeze(x, 0)
+        ########################################################################
+        # TODO: Define the forward pass behavior of your model                 #
+        # for an input image x, forward(x) should return the                   #
+        # corresponding predicted keypoints.                                   #
+        # NOTE: what is the required output size?                              #
+        ########################################################################
+
+        x = self.model(x)
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return x
+
+    def training_step(self, batch, batch_idx):
+        image, keypoints = batch["image"], batch["keypoints"]
+
+        # Perform a forward pass on the network with inputs
+        out = self.forward(image).view(-1, 15, 2)
+
+        # calculate the loss with the network predictions and ground truth targets
+        loss_fn = nn.MSELoss()
+
+        loss = loss_fn(keypoints, out)
+
+        #self.log('loss', loss)
+        #return {'loss': loss}
+        return loss
+
+    def configure_optimizers(self):
+        optim = torch.optim.Adam(self.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08)
+        return optim
+
+
+class DummyKeypointModel(pl.LightningModule):
+    """Dummy model always predicting the keypoints of the first train sample"""
+    def __init__(self):
+        super().__init__()
+        self.prediction = torch.tensor([[
+            0.4685, -0.2319,
+            -0.4253, -0.1953,
+            0.2908, -0.2214,
+            0.5992, -0.2214,
+            -0.2685, -0.2109,
+            -0.5873, -0.1900,
+            0.1967, -0.3827,
+            0.7656, -0.4295,
+            -0.2035, -0.3758,
+            -0.7389, -0.3573,
+            0.0086, 0.2333,
+            0.4163, 0.6620,
+            -0.3521, 0.6985,
+            0.0138, 0.6045,
+            0.0190, 0.9076,
+        ]])
+
+    def forward(self, x):
+        return self.prediction.repeat(x.size()[0], 1, 1, 1)
